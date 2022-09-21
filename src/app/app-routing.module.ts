3469c7570eea7d9f8409d0628b087e996d1469ba
sk9797 <siddheshkamble9797@gmail.com>
2022-09-21 18:14:53 +0530

    init commit with comps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { SettingsPageComponent } from './components/settings-page/settings-page.component';
+import { UserPageComponent } from './components/user-page/user-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'user', component: UserPageComponent },
+  { path: 'settings', component: SettingsPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { UserPageComponent } from './components/user-page/user-page.component';
+import { SettingsPageComponent } from './components/settings-page/settings-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    UserPageComponent,
+    SettingsPageComponent
   ],
   imports: [
     BrowserModule,
